28366500b2cbd7c110374e8d2a132895e4286656
felipeOvr <gm.felipeoliveira@gmail.com>
2019-10-27 17:34:52 -0300

    aula 40/59

---
+++ controllers/UserController.js
@@ -0,0 +1,86 @@
+class UserController
+{
+    constructor (formID, tableID)
+    {
+        this.formEl = document.getElementById (formID)
+        this.tableEl = document.getElementById (tableID)
+
+        this.submitListener()
+    }
+
+    submitListener ()
+    {
+        this.formEl.addEventListener ('submit', event => {
+
+            event.preventDefault()
+
+            let fields = this.getFields ()
+
+            this.getPhoto( photoContent => {
+
+                fields.photo = photoContent
+                this.addLine (fields)
+            })
+        })
+
+    } // submitListener
+
+    getPhoto (callBack)
+    {
+        let fileReader = new FileReader ()
+
+        let photoElement = [...this.formEl.elements].filter (item => item.name === 'photo')
+
+        let photo = photoElement [0].files [0]
+
+        fileReader.onload = () => callBack (fileReader.result)
+
+        fileReader.readAsDataURL (photo)
+    }
+
+    getFields ()
+    {
+        let user = {} ;
+
+        [...this.formEl.elements].forEach ( (field, index) => {
+
+            if (field.name == "gender")
+            {
+                if (field.checked) user[field.name] = field.value
+            }
+            else
+            {
+                user[field.name] = field.value
+            }
+        } )
+    
+        return new User (
+            user.name,
+            user.gender,
+            user.birth,
+            user.country,
+            user.email,
+            user.password,
+            user.photo,
+            user.admin
+        )
+
+    } // getFields
+
+    addLine (userData)
+    {
+        this.tableEl.innerHTML = `<tr>
+
+            <td><img src="${ userData.photo }" alt="User Image" class="img-circle img-sm"></td>
+            <td>${ userData.name }</td>
+            <td>${ userData.email }</td>
+            <td>${ userData.admin }</td>
+            <td>${ userData.birth }</td>
+            <td>
+                <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+                <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+            </td>
+        </tr>`
+
+    } // addLine
+}
--- src/app.js
+++ src/app.js
@@ -1,12 +1,1 @@
-const userForm = document.querySelector('#form-user-create')
-
-const inputName = document.querySelector('#exampleInputName')
-
-const inputGender = document.querySelectorAll('#form-user-create [name=gender]:checked')
-
-const inputBirth = document.querySelector('#exampleInputBirth')
-const inputCountry = document.querySelector('#exampleInputCountry')
-const inputEmail = document.querySelector('#exampleInputEmail1')
-const inputPassword = document.querySelector('#exampleInputPassword1')
-const inputPhoto = document.querySelector('#exampleInputFile')
-const inputAdmin = document.querySelector('#exampleInputAdmin')
+const userController = new UserController ('form-user-create', 'table-users')
